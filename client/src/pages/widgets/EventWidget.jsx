f1f73d0204e113b5d670c18d6a78774d0343b908
Akhil Eldho <akhil.p.eldho@gmail.com>
2023-06-18 15:40:42 +1200

    funky

--- client/src/pages/widgets/EventWidget.jsx
+++ client/src/pages/widgets/EventWidget.jsx
@@ -1,6 +1,28 @@
 import { Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
+import Question from "./Question";
+
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  }
+];
 
 //Event widget
 const EventWidget = () => {
@@ -25,6 +47,17 @@ const EventWidget = () => {
         src="http://localhost:8800/assets/Graduation.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
+
+
+      <div className="container">
+        <h2>Frequently Asked Questions</h2>
+        <div className="questions">
+          {questions.map((question) => (
+            <Question key={question.id} question={question} />
+          ))}
+        </div>
+      </div>
+
       <FlexBetween>
         <Typography color={main}>Student Space</Typography>
         <Typography color={medium}>StudentSpace.com</Typography>
